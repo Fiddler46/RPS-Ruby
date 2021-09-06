54a49236416d30141d459e66b0b924b5518e8595
Fiddler46 <adhyaksh99@gmail.com>
2021-09-06 18:52:35 +0530

    Conditions for printing winner.

--- rps.rb
+++ rps.rb
@@ -1,22 +1,34 @@
 class RPS
-    attr_writer :seed
+    attr_writer :seed   
 
-    def initialize(guess:)
+    def initialize(guess:)  
         @guess = guess.capitalise
     end        
 
-    def rand_algo
+    def rand_algo   
         rand 12345
     end
 
-    def rule_engine
+    def rule_engine 
         {
             "Rock" => ["Scissors"],
             "Paper" => ["Rock"],
             "Scissors" => ["Paper"]
         }
+    end
+
     def sys_guess
         srand (@random || rand_algo)
         sys_guesses = %w{Rock Paper Scissors}
         sys_guesses.sample
+    end
+
+    def winner_is
+        if rule_engine [sys_guess].include? @guess
+          'Computer wins'
+        elsif rule_engine[@guess].include? sys_guess
+          'You win!'
+        else
+          'Tie'
+        end
     end
