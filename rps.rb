71951df0673cb5270347827a7970670268edcbd2
Fiddler46 <adhyaksh99@gmail.com>
2021-09-07 16:29:57 +0530

    Added random seed to test for control

--- rps.rb
+++ rps.rb
@@ -1,36 +1,43 @@
 class RPS
-
+    attr_writer :seed
     def initialize(guess:)  
         @guess = guess.capitalize
     end        
 
+    def rand_algo
+      rand 12345
+      
+    end  
+
     def rule_engine 
         {
-            'Rock': ['Scissors'],
-            'Paper': ['Rock'],
-            'Scissors': ['Paper']
+            'Rock'=> ['Scissors'],
+            'Paper'=> ['Rock'],
+            'Scissors'=> ['Paper']
         }
     end
 
     def sys_guess
-        rand 12345
+        srand (@seed || rand_algo)
         sys_guesses = %w{Rock Paper Scissors}
         sys_guesses.sample
     end
 
     def winner_is 
-         puts sys_guess.to_s
-         puts @guess.to_s
+      puts "You guessed #{@guess.to_s}"   
+      puts "Computer guessed #{sys_guess.to_s}"
+         
 
-        if rule_engine[sys_guess].include? @guess
-           puts "Computer wins"
-         elsif rule_engine[@guess].include? sys_guess
-           puts "You win!"
-         else
-           puts "Tie"
-         end
+      if rule_engine[sys_guess].include? @guess
+         puts "Computer wins"
+      elsif rule_engine[@guess].include? sys_guess
+         puts "You win!"
+      else
+        puts "Tie"
+       end
     end
 end
 
-rps = RPS.new(guess: gets)
+rps = RPS.new(guess: gets.chomp!)
+#rps.seed = 1
 rps.winner_is
