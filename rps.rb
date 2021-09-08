cc00ccb1b3af8876cbf0ef62ddb81b989f1d19bc
Fiddler46 <adhyaksh99@gmail.com>
2021-09-08 16:36:23 +0530

    Checked for input validity and added comments

--- rps.rb
+++ rps.rb
@@ -1,43 +1,52 @@
 class RPS
-    attr_writer :seed
+    attr_writer :seed # Seed attribute to control what the system guesses
     def initialize(guess:)  
         @guess = guess.capitalize
     end        
 
-    def rand_algo
+    def rand_algo # Randomisation algorithm 
       rand 12345
       
     end  
 
-    def rule_engine 
+    def rule_engine # Hash that contains key-value pairs of possible winning combinations for RPS
         {
-            'Rock'=> ['Scissors'],
-            'Paper'=> ['Rock'],
-            'Scissors'=> ['Paper']
+            'Rock' => ['Scissors'],
+            'Paper' => ['Rock'],
+            'Scissors' => ['Paper']
         }
     end
 
-    def sys_guess
+    def sys_guess # Takes in a control seed to guess the same value or randomly selects one from the given array
         srand (@seed || rand_algo)
         sys_guesses = %w{Rock Paper Scissors}
         sys_guesses.sample
     end
 
-    def winner_is 
-      puts "You guessed #{@guess.to_s}"   
-      puts "Computer guessed #{sys_guess.to_s}"
-         
+    def winner_is # Checks and compares input vs system guess to see who won or lost or if it's a tie
+      sg = sys_guess
+      puts "You guessed #{@guess}"   
+      puts "Computer guessed #{sg}"
 
-      if rule_engine[sys_guess.to_s].include? @guess
-         puts "Computer wins"
-      elsif rule_engine[@guess].include? sys_guess
-         puts "You win!"
+      if rule_engine.key?(@guess)
+        if rule_engine[sg].include? @guess
+          puts "Computer wins"
+        elsif rule_engine[@guess].include? sg
+          puts "You win!"
+        else
+          puts "Tie"
+        end
       else
-        puts "Tie"
-       end
+        puts "Incorrect guess, try again!"
+      end
+    
     end
-end
+  
+  end
 
 rps = RPS.new(guess: gets.chomp!)
-#rps.seed = 3
+#rps.seed = 2 
 rps.winner_is
+  
+
+  
